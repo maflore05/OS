e562756c04bca744f5170688a1ff7853b3bda352
Brian <69977805+BrainMon@users.noreply.github.com>
2024-11-03 00:26:46 -0600

    Update recieve_udp.c
    Final edit, removed printf for write, better error handling.

--- Homework_2/recieve_udp.c
+++ Homework_2/recieve_udp.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 65536
 
@@ -28,34 +28,6 @@ static int convert_port_name(uint16_t *port, const char *port_name) {
     return 0;
 }
 
-ssize_t better_write(int fd, const char *buf, size_t count) {
-  size_t already_written, to_be_written, written_this_time, max_count;
-  ssize_t res_write;
-
-  if (count == ((size_t) 0)) return (ssize_t) count;
-  
-  already_written = (size_t) 0;
-  to_be_written = count;
-  while (to_be_written > ((size_t) 0)) {
-    max_count = to_be_written;
-    if (max_count > ((size_t) 8192)) {
-      max_count = (size_t) 8192;
-    }
-    res_write = write(fd, &(((const char *) buf)[already_written]), max_count);
-    if (res_write < ((size_t) 0)) {
-      /* Error */
-      return res_write;
-    }
-    if (res_write == ((ssize_t) 0)) {
-      /* Nothing written, stop trying */
-      return (ssize_t) already_written;
-    }
-    written_this_time = (size_t) res_write;
-    already_written += written_this_time;
-    to_be_written -= written_this_time;
-  }
-  return (ssize_t) already_written;
-}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -64,10 +36,11 @@ int main(int argc, char *argv[]) {
     }
 
     int sockfd;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in servaddr, clientaddr;
     uint16_t port;
     char buffer[BUFFER_SIZE];
     ssize_t recv_bytes;
+    socklen_t addrlen = sizeof(clientaddr);
 
     // Convert port name to 16-bit unsigned integer
     if (convert_port_name(&port, argv[1]) != 0) {
@@ -94,25 +67,24 @@ int main(int argc, char *argv[]) {
         close(sockfd);
         return -1;
     }
+    
+    write(1, "reply_udp is running on port: ", 31);
+    write(1, argv[1], 4);
+    write(1, "\n", 2);
 
-    // Continuously receive packets until a 0-byte UDP packet is received
+    // Main loop to receive and reply to incoming packets
     while (1) {
-        recv_bytes = recv(sockfd, buffer, BUFFER_SIZE, 0);
+        // Receive a packet from the client
+        recv_bytes = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientaddr, &addrlen);
         if (recv_bytes < 0) {
-            fprintf(stderr, "recv Error: %s\n", strerror(errno));
+            fprintf(stderr, "recvfrom Error: %s\n", strerror(errno));
             close(sockfd);
             return -1;
         }
 
-        // If a 0-byte packet is received, terminate the program
-        if (recv_bytes == 0) {
-            better_write(1, "Received 0-byte packet. Terminating.\n", 38);
-            break;
-        }
-
-        // Write the received data to standard input. 
-        if (better_write(STDOUT_FILENO, buffer, recv_bytes) < 0) {
-            fprintf(stderr, "write Error: %s\n", strerror(errno));
+        // Send the received packet back to the client
+        if (sendto(sockfd, buffer, recv_bytes, 0, (struct sockaddr *)&clientaddr, addrlen) != recv_bytes) {
+            fprintf(stderr, "sendto: %s\n", strerror(errno));
             close(sockfd);
             return -1;
         }
